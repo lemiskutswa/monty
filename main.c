d4c7a6982f59fe524ddb76e3be95466fea9d1005
lemiskutswa <kutswalemis16@gmail.com>
2022-06-16 12:00:24 +0300

    monty fix 2

--- main.c
+++ main.c
@@ -1,103 +1,47 @@
 #include "monty.h"
 
+vars var;
 
 /**
-* opcode_finder - find opcode
-* @stack: stack pointer
-* @opcode: user input opcode
-* @line_number: line number
-* Return: Always 1 (Success) or stderr
-**/
-int find_opcode(stack_t **stack, char *opcode, int line_number)
+ * main - Start LIFO, FILO program
+ * @ac: Number of arguments
+ * @av: Pointer containing arguments
+ * Return: 0 Success, 1 Failed
+ */
+int main(int ac, char **av)
 {
-instruction_t opcodes[] = {
-{"pall", pall},
-{"pop", pop},
-{"swap", swap},
-{"pint", pint},
-{NULL, NULL}
-};
+	char *opcode;
 
-int i;
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-for (i = 0; opcodes[i].opcode; i++)
-{
-if (strcmp(opcode, opcodes[i].opcode) == 0)
-{
-(opcodes[i].f)(stack, line_number);
-return (EXIT_SUCCESS);
-}
-}
-fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-exit(EXIT_FAILURE);
-}
+	if (start_vars(&var) != 0)
+		return (EXIT_FAILURE);
 
+	var.file = fopen(av[1], "r");
+	if (!var.file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		free_all();
+		return (EXIT_FAILURE);
+	}
 
-/**
-* main - Process Monty byte codes from a file passed in as an argument
-* @argc: size of argv
-* @argv: A double pointer contain the arguments
-* Return: EXIT_SUCCESS if no errors or EXIT_FAILURE
-**/
+	while (getline(&var.buff, &var.tmp, var.file) != EOF)
+	{
+		opcode = strtok(var.buff, " \r\t\n");
+		if (opcode != NULL)
+			if (call_funct(&var, opcode) == EXIT_FAILURE)
+			{
+				free_all();
+				return (EXIT_FAILURE);
+			}
+		var.line_number++;
+	}
 
-int main(__attribute__((unused)) int argc, char const *argv[])
-{
-FILE *mf;
-char *buff = NULL, *opcode, *n;
-size_t lol = 0;
-int line_number = 0;
-stack_t *stack = NULL, *current;
+	free_all();
 
-if (argc != 2)
-{
-fprintf(stderr, "USAGE: monty file\n");
-return (EXIT_FAILURE);
-}
-mf = fopen(argv[1], "r");
-if (mf == NULL)
-{
-fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-exit(1);
-}
-while ((getline(&buff, &lol, mf)) != -1)
-{
-line_number++;
-opcode = strtok(buff, DELIMATOR);
-if (opcode == NULL || opcode[0] == '#')
-continue;
-if (!strcmp(opcode, "nop"))
-continue;
-else if (!strcmp(opcode, "push"))
-{
-n = strtok(NULL, DELIMATOR);
-push(&stack, n, line_number);
-}
-else
-find_opcode(&stack, opcode, line_number);
-}
-fclose(mf);
-free(buff);
-while (stack != NULL)
-{
-current = stack;
-stack = stack->next;
-free(current);
-}
-return (0);
-}
-
-/**
-* free_stack - fff
-* @stack: fff
-**/
-void free_stack(stack_t *stack)
-{
-stack_t *next;
-
-while (stack != NULL)
-{
-next = stack->next;
-free(stack);
-stack = next;
-}
+	return (EXIT_SUCCESS);
 }
